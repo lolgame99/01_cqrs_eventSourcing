f22cae051d20fc47926f9e49f0f4935718ba1fff
JesterJesterson <dreher-fabian@web.de>
2022-04-04 10:19:13 +0200

    BookingCreatedEvent added

---
+++ src/main/java/eventside/domain/BookingCancelledEvent.java
@@ -0,0 +1,40 @@
+package eventside.domain;
+
+import eventside.domain.Event;
+import writeside.domain.Booking;
+import writeside.domain.PersonalDetails;
+import writeside.domain.Room;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.Set;
+
+public class BookingCancelledEvent extends Event {
+
+
+    private String customer;
+    private long timestamp;
+    private String content;
+
+    public BookingCancelledEvent(String customer, long timestamp, String content) {
+        this.customer = customer;
+        this.timestamp = timestamp;
+        this.content = content;
+
+
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
